8a2e4aaa0bfebb49a894a67ebd0bdf365b2d9f49
tonyg92x <anthony.guay112@gmail.com>
2023-03-24 10:56:06 -0400

    CPP09 done

--- 10-piscineCPP/CPP09/ex02/includes/main.hpp
+++ 10-piscineCPP/CPP09/ex02/includes/main.hpp
@@ -1,3 +1,47 @@
 #pragma once
 
-#include <stdio.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <deque>
+#include <stdexcept>
+#include <algorithm>
+#include <iterator>
+#include <sys/time.h>
+
+/*  Insert Merge sort algorithm
+
+    It use the merge sort algorithm, but add a insert algorithm
+    to containers which are smaller than a given point, in this case 10.
+    It avoids spliting in two multiple time since you don't have to reach
+    the container size of one for each container.
+*/
+template <typename Container>
+void mergeInsertSort(Container& container)
+{
+    if (container.size() <= 1)
+        return;
+
+    if (container.size() < 10)
+    {
+        std::sort(container.begin(), container.end());
+        return;
+    }
+
+    // Split the container into two halves
+    typename Container::iterator middle = container.begin();
+    std::advance(middle, container.size() / 2);
+    Container left(container.begin(), middle);
+    Container right(middle, container.end());
+
+    // Recursively sort each half
+    mergeInsertSort(left);
+    mergeInsertSort(right);
+
+    // Merge the two sorted halves into a new container
+    Container result;
+    std::merge(left.begin(), left.end(), right.begin(), right.end(), std::back_inserter(result));
+
+    // Assign the sorted elements back to the original container
+    container.assign(result.begin(), result.end());
+}
--- 10-piscineCPP/CPP09/ex02/srcs/main/main.cpp
+++ 10-piscineCPP/CPP09/ex02/srcs/main/main.cpp
@@ -3,17 +3,82 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: anthony <anthony@student.42.fr>            +#+  +:+       +#+        */
+/*   By: aguay <aguay@42student.fr>                 +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/24 13:20:00 by aguay             #+#    #+#             */
-/*   Updated: 2022/06/14 08:10:50 by anthony          ###   ########.fr       */
+/*   Updated: 2023/03/24 10:52:25 by aguay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "main.hpp"
 
-int	main(void)
+class MergeSortException : public std::exception
 {
-	printf("Hello World!\n");
-	return (0);
+    public:
+        const char* what() const throw()
+        {
+            return "Error";
+        }
+};
+
+int main(int argc, char* argv[])
+{
+    std::vector<int>    numbers;
+    std::vector<int>    sortedVector;
+    std::deque<int>     sortedDeque;
+    struct timeval      start;
+    struct timeval      end;
+
+    try
+    {
+        if (argc < 2)
+            throw MergeSortException();
+
+        for (int i = 1; i < argc; ++i)
+        {
+            int number = std::atoi(argv[i]);
+            if (number < 1)
+                throw MergeSortException();
+            numbers.push_back(number);
+        }
+
+        // Display the unsorted sequence
+        std::cout << "Before: ";
+        for (std::vector<int>::iterator it = numbers.begin(); it != numbers.end(); it++)
+            std::cout << *it << ' ';
+        std::cout << std::endl; 
+
+        // Sort the vector
+        sortedVector = numbers;
+        gettimeofday(&start, NULL);
+        mergeInsertSort(sortedVector);
+        gettimeofday(&end, NULL);
+        float elapsed_time = end.tv_usec - start.tv_usec;
+
+        // Display the sorted vector
+        std::cout << "After: ";
+        for (std::vector<int>::iterator it = sortedVector.begin(); it != sortedVector.end(); it++)
+            std::cout << *it << ' ';
+        std::cout << std::endl; 
+
+        // Display the time of the process.
+        std::cout << "Time to process a range of " << numbers.size() << " elements with std::vector:\t" << elapsed_time << " ms" << std::endl;
+
+        // Sort the sequence using std::deque
+        sortedDeque.assign(numbers.begin(), numbers.end());
+        gettimeofday(&start, NULL);
+        mergeInsertSort(sortedDeque);
+        gettimeofday(&end, NULL);
+        elapsed_time = end.tv_usec - start.tv_usec;
+
+        //  Display the time of the process.
+        std::cout << "Time to process a range of " << numbers.size() << " elements with std::deque:\t" << elapsed_time << " ms" << std::endl;
+    }
+    catch (const MergeSortException& ex)
+    {
+        std::cerr << "Error" << std::endl;
+        return 1;
+    }
+
+    return 0;
 }
